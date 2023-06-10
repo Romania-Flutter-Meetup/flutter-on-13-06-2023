93c567649c62ec93f0ab341101d0b58e3290ecb3
Vlad Rosca <vladrosca93@gmail.com>
2023-06-10 18:19:06 +0300

    Added a bit more diversity

--- lib/common/to_do_item.dart
+++ lib/common/to_do_item.dart
@@ -24,60 +24,72 @@ class _ToDoItemState extends State<ToDoItem> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Checkbox(
-              value: widget.toDo.isDone,
-              onChanged: (bool? value) {
-                context.read<TodoBloc>().add(UpdateTodo(
-                    todo: ToDo(
-                        id: widget.toDo.id,
-                        message: widget.toDo.message,
-                        isDone: value ?? false)));
-              }),
-          Flex(
-            direction: Axis.horizontal,
-            children: <Widget>[
-              Text(
-                widget.toDo.message,
-                style: TextStyle(
-                    fontSize: 16,
-                    decoration: widget.toDo.isDone
-                        ? TextDecoration.lineThrough
-                        : TextDecoration.none),
-              )
-            ],
-          ),
-          IconButton(
-              onPressed: () {
-                showModal(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Are you sure'),
-                        content: const Text('Are you sure you want to delete?'),
-                        actions: <Widget>[
-                          TextButton(
-                              onPressed: () {
-                                context
-                                    .read<TodoBloc>()
-                                    .add(DeleteTodo(todo: widget.toDo));
-                                    Navigator.of(context).pop();
-                              },
-                              child: const Text('Yes')),
-                          TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text('No'))
-                        ],
-                      );
-                    });
-              },
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.red,
-              ))
+          _buildCheckBox(context),
+          _buildDisplayedText(),
+          _buildDeleteIcon(context)
         ],
       ),
     );
+  }
+
+  IconButton _buildDeleteIcon(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          showModal(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Are you sure'),
+                  content: const Text('Are you sure you want to delete?'),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () {
+                          context
+                              .read<TodoBloc>()
+                              .add(DeleteTodo(todo: widget.toDo));
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('Yes')),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('No'))
+                  ],
+                );
+              });
+        },
+        icon: const Icon(
+          Icons.delete,
+          color: Colors.red,
+        ));
+  }
+
+  Flex _buildDisplayedText() {
+    return Flex(
+      direction: Axis.horizontal,
+      children: <Widget>[
+        Text(
+          widget.toDo.message,
+          style: TextStyle(
+              fontSize: 16,
+              decoration: widget.toDo.isDone
+                  ? TextDecoration.lineThrough
+                  : TextDecoration.none),
+        )
+      ],
+    );
+  }
+
+  Checkbox _buildCheckBox(BuildContext context) {
+    return Checkbox(
+        value: widget.toDo.isDone,
+        onChanged: (bool? value) {
+          context.read<TodoBloc>().add(UpdateTodo(
+              todo: ToDo(
+                  id: widget.toDo.id,
+                  message: widget.toDo.message,
+                  isDone: value ?? false)));
+        });
   }
 }
