b494d090f214a28bb84d8c6d521921cbf31cb035
Vlad Rosca <vladrosca93@gmail.com>
2023-06-10 18:23:04 +0300

    Also added images

--- lib/assets/assets.dart
+++ lib/assets/assets.dart
@@ -1,5 +1,9 @@
 class Assets {
+  //?-------------ANIMATIONS
   static const String INTRO_ANIMATION = 'assets/animations/intro.json';
   static const String DONE_ANIMATION = 'assets/animations/done.json';
   static const String NO_ITEMS = 'assets/animations/no_items.json';
+
+  //?--------------IMAGES
+  static const String LOGO = 'assets/images/logo.png';
 }
